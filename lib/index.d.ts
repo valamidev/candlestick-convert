7a8a22eaf269b585805ae6642e63fedce326d089
Valamidev <Valamidev@users.noreply.github.com>
2019-08-30 09:30:21 +0200

    Typescript rewrite and v4.0 changes

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,5 @@
 import { candletick } from "./interfaces";
-export declare const convert: {
+export declare const Converter: {
     candle_batch: (candledata: candletick[], baseFrame?: number, newFrame?: number) => candletick[];
     trade_to_candle: (tradedata: any, intverval?: number) => any[];
     tick_chart: (tradedata: any, tick_lenght?: number) => any[];
