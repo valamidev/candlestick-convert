c9990f9f84e8c91952ce00a6e64dbe3b6e381067
T <52665225+valamidev@users.noreply.github.com>
2020-05-11 10:10:08 +0200

    Fix batchCandleArray() and refactor batchCandleJSON - batchCandleJSON no longer hold independent logic only a wrapper for batchCandleArray(). - Tests are now verified with Binance candle values

--- src/index.ts
+++ src/index.ts
@@ -37,25 +37,22 @@ export type Trade = TradeTick;
 export const batchCandleArray = (candledata: OHLCV[],
   baseFrame: number = 60,
   newFrame: number = 300): OHLCV[] => {
-
+  const result: OHLCV[] = [];
   baseFrame *= 1000;
   newFrame *= 1000;
 
-  let convertRatio = Math.floor(newFrame / baseFrame);
+  const convertRatio = Math.floor(newFrame / baseFrame);
 
-  let result: OHLCV[] = [];
-
-  if (convertRatio < 1) {
-    throw new Error("Convert cannot be smaller than 1");
+  if (convertRatio % 1 !== 0) {
+    throw new Error("Convert ratio should integer an >= 2");
   }
 
-  // Candledata Array check
   if (Array.isArray(candledata)) {
     if (candledata.length == 0 || candledata.length < convertRatio) {
       return result;
     }
   } else {
-    throw new Error("Candledata is not an array!");
+    throw new Error("Candledata is empty or not an array!");
   }
 
   // Sort Data to ascending by Time
@@ -67,11 +64,9 @@ export const batchCandleArray = (candledata: OHLCV[],
   let close = 0;
   let low = 0;
   let volume = 0;
-  let timeOpen = 0;
+  let timeOpen = null;
   let j = 0;
 
-  // Candledata [time,open,high,low,close,volume]
-  //              0    1    2   3    4     5
   for (let i = 0; i < candledata.length; i++) {
     const candle = candledata[i];
 
@@ -83,44 +78,53 @@ export const batchCandleArray = (candledata: OHLCV[],
     candle[OHLCVField.CLOSE] = Number(candle[OHLCVField.CLOSE]);
     candle[OHLCVField.VOLUME] = Number(candle[OHLCVField.VOLUME]);
 
-    //Get open values
-    if (j == 0) {
+
+    // First / Force New Candle
+    if(timeOpen === null){
       timeOpen = candle[OHLCVField.TIME];
-      open = candle[OHLCVField.OPEN];
-      low = candle[OHLCVField.LOW];
-      volume = candle[OHLCVField.VOLUME];
-    } else {
-      // Add volume non open step
-      volume += candle[OHLCVField.VOLUME];
-    }
-    // Count timeframe size
-    j++;
 
-    // Calculate low
-    low = Math.min(candle[OHLCVField.LOW], low);
-    // Calculate high
-    high = Math.max(candle[OHLCVField.HIGH], high);
-
-    // If we have too many skipped candle
-    if (candle[OHLCVField.TIME] - timeOpen >= newFrame) {
-      open = candle[OHLCVField.OPEN];
-      low = candle[OHLCVField.LOW];
-      high = candle[OHLCVField.HIGH];
-      volume = candle[OHLCVField.VOLUME];
-    }
-
-    // When time is match with Time frame or we match the frame_lenght
-    if (candle[OHLCVField.TIME] % newFrame == 0 || j >= convertRatio) {
-      // Add result only if we match time frame
-      if (candle[OHLCVField.TIME] % newFrame == 0) {
-        // Close is always close
-        close = candle[OHLCVField.CLOSE];
-        result.push([candle[OHLCVField.TIME], open, high, low, close, volume]);
+      if(candle[OHLCVField.TIME] % newFrame > 0){
+        timeOpen = candle[OHLCVField.TIME] - candle[OHLCVField.TIME] % newFrame;
       }
 
-      // Reset buffers
-      open = high = close = low = volume = j = 0;
+      open = candle[OHLCVField.OPEN];
+      high = candle[OHLCVField.HIGH];
+      low = candle[OHLCVField.LOW];
+      close = candle[OHLCVField.CLOSE];
+      volume = 0;
+      j = 1;
+      
     }
+
+      // New Candle
+    if( candle[OHLCVField.TIME] - candle[OHLCVField.TIME] % newFrame !== timeOpen){
+
+      result.push([timeOpen, open, high, low, close, volume]);
+
+      timeOpen = candle[OHLCVField.TIME] - candle[OHLCVField.TIME] % newFrame;
+      open = candle[OHLCVField.OPEN];
+      high = candle[OHLCVField.HIGH];
+      low = candle[OHLCVField.LOW];
+      close = candle[OHLCVField.CLOSE];
+      volume = 0;
+      j = 1;
+    }
+
+    high = Math.max(candle[OHLCVField.HIGH], high);
+    low = Math.min(candle[OHLCVField.LOW], low);
+    close = candle[OHLCVField.CLOSE];
+    volume =  volume + candle[OHLCVField.VOLUME];
+    
+    // Batch counter
+    
+
+    if(j === convertRatio){
+      result.push([timeOpen, open, high, low, close, volume]);
+      timeOpen = null;
+    }
+  
+    j = j+1;
+
   }
 
   return result;
@@ -129,92 +133,19 @@ export const batchCandleArray = (candledata: OHLCV[],
 
 
 export const batchCandleJSON = (candledata: IOHLCV[], baseFrame = 60, newFrame = 300): IOHLCV[] => {
-  baseFrame *= 1000;
-  newFrame *= 1000;
 
-  let convertRatio = Math.floor(newFrame / baseFrame);
+  const ohlcvArray: OHLCV[] = candledata.map(e => [e.time,e.open,e.high,e.low,e.close,e.volume]);
 
-  let result: IOHLCV[] = [];
+  const batchedOhlcvArray = batchCandleArray(ohlcvArray,baseFrame,newFrame);
 
-  if (convertRatio < 1) {
-    throw "Target time frame must be a positive multiple of original timeframe";
-  }
-
-  // Candledata Array check
-  if (Array.isArray(candledata)) {
-    if (candledata.length == 0 || candledata.length < convertRatio) {
-      return result;
-    }
-  } else {
-    throw new Error("Candledata is not an array!");
-  }
-
-  // Sort Data to ascending
-  candledata.sort((a, b) => a.time - b.time);
-
-  // Buffer values
-  let open = 0;
-  let high = 0;
-  let close = 0;
-  let low = 0;
-  let volume = 0;
-  let timeOpen = 0;
-  let j = 0;
-
-  // Candledata [time,open,high,low,close,volume]
-  //              0    1    2   3    4     5
-  for (let i = 0; i < candledata.length; i++) {
-    const candle = candledata[i];
-
-    // Type convert
-    candle.time = Number(candle.time);
-    candle.open = Number(candle.open);
-    candle.high = Number(candle.high);
-    candle.low = Number(candle.low);
-    candle.close = Number(candle.close);
-    candle.volume = Number(candle.volume);
-
-    //Get open values
-    if (j == 0) {
-      timeOpen = candle.time;
-      open = candle.open;
-      low = candle.low;
-      volume = candle.volume;
-    } else {
-      // Add volume non open step
-      volume += candle.volume;
-    }
-    // Count timeframe size
-    j++;
-
-    // Calculate low
-    low = Math.min(candle.low, low);
-    // Calculate high
-    high = Math.max(candle.high, high);
-
-    // If we have too many skipped candle
-    if (candle.time - timeOpen >= newFrame) {
-      open = candle.open;
-      low = candle.low;
-      high = candle.high;
-      volume = candle.volume;
-    }
-
-    // When time is match with Timeframe or we match the
-    if (candle.time % newFrame == 0 || j >= convertRatio) {
-      // Add result only if we match time frame
-      if (candle.time % newFrame == 0) {
-        // Close is always close
-        close = candle.close;
-        result.push({ time: candle.time, open, high, low, close, volume });
-      }
-
-      // Reset buffers
-      open = high = close = low = volume = j = 0;
-    }
-  }
-
-  return result;
+  return batchedOhlcvArray.map(candle => ({
+    time: candle[OHLCVField.TIME],
+    open: candle[OHLCVField.OPEN],
+    high:candle[OHLCVField.HIGH],
+    low: candle[OHLCVField.LOW],
+    close: candle[OHLCVField.CLOSE],
+    volume: candle[OHLCVField.VOLUME],
+  }));
 }
 
 
