7b98e58d61e13393e5f07fa7bc7847b90686931d
T <52665225+valamidev@users.noreply.github.com>
2023-04-07 09:17:25 +0200

    Update devDependencies

--- jest.config.js
+++ jest.config.js
@@ -2,16 +2,16 @@ module.exports = {
   globals: {
     "ts-jest": {
       diagnostics: {
-        ignoreCodes: [2345]
-      }
-    }
+        ignoreCodes: [2345],
+      },
+    },
   },
   collectCoverage: true,
-  coveragePathIgnorePatterns: ['/node_modules|dist/'],
-  collectCoverageFrom: ['src/**/*.ts'],
+  coveragePathIgnorePatterns: ["/node_modules|dist/"],
+  collectCoverageFrom: ["src/**/*.ts"],
   transform: {
-    '.(ts|tsx)': 'ts-jest',
+    ".(ts|tsx)": "ts-jest",
   },
-  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
-  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  testRegex: "(/__tests__/.*|\\.(test|spec))\\.(ts)$",
+  moduleFileExtensions: ["js", "json", "jsx", "ts", "tsx", "node"],
 };
